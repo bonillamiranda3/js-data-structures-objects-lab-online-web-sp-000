f0c7d8d6847a2cba3d37f54260e2810b935dae2a
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-02-03 04:49:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,20 @@
 const driver = {
 };
 
-function updateDriverWithKeyAndValue(driver, key, value)  {
-  return object.assign({}, driver, {[key]:value});
+function updateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign({}, driver, {[key]:value});
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign(driver, {[key]: value});
+}
+
+function deleteFromDriverByKey(driver, key) { 
+  new_driver = Object.assign({}, driver) ; 
+  delete new_driver[key]; 
+  return new_driver; 
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){ 
+  delete driver[key]; 
+  return driver; }
